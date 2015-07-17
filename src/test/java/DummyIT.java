4c9d119acdf54e4a728960501a4c40aa7879b33a
ptrouillard <ptrouillard@gmail.com>
2015-07-17 12:31:08 +0200

    provide a sample solution with jetty for IT tests

---
+++ src/test/java/DummyIT.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by ptrouillard on 17/07/2015.
+ */
+public class DummyIT {
+
+    @Test
+    public void verify_sample_page() {
+
+        // do whatever you want to test the webapp here...
+        System.out.println("Dummy test launched !");
+    }
+
+}
